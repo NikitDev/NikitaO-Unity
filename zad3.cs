3b493c8ae9ef81c22866b55d0c392601d0fda30c
NikitDev <91266814+NikitDev@users.noreply.github.com>
2023-11-06 18:56:21 +0100

    Update zad3.cs

--- zad3.cs
+++ zad3.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class zad3 : MonoBehaviour
 {
-    public float speed = 10.0f; // Publiczne pole do ustawienia prêdkoœci ruchu
-    public float rotationSpeed = 90.0f; // Publiczne pole do ustawienia prêdkoœci obrotu
+    public float speed = 10.0f;
+    public float rotationSpeed = 90.0f;
 
     private Vector3 initialPosition;
-    private float moveDistance = 10.0f; // Odleg³oœæ, po której obiekt siê przemieszcza
+    private float moveDistance = 10.0f;
     private bool isTurning = false;
     private Vector3 targetRotation;
 
@@ -22,10 +22,7 @@ public class zad3 : MonoBehaviour
     {
         if (!isTurning)
         {
-            // Przesuñ obiekt wzd³u¿ osi X
             transform.Translate(Vector3.right * speed * Time.deltaTime);
-
-            // SprawdŸ, czy obiekt przekroczy³ okreœlon¹ odleg³oœæ
             if (Vector3.Distance(initialPosition, transform.position) >= moveDistance)
             {
                 isTurning = true;
@@ -33,10 +30,7 @@ public class zad3 : MonoBehaviour
         }
         else
         {
-            // Obracaj obiekt o 90 stopni w kierunku kolejnego ruchu
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), rotationSpeed * Time.deltaTime);
-
-            // SprawdŸ, czy obiekt obróci³ siê o 90 stopni
             if (Quaternion.Angle(transform.rotation, Quaternion.Euler(targetRotation)) < 1.0f)
             {
                 isTurning = false;
